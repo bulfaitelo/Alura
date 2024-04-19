bf3cd299cbae471fe1c28439c6587619ef77a1d6
Thiago Rodrigues <bulfaitelo@gmail.com>
2024-04-19 11:07:33 -0300

    Finalizando aula Primeiro programa em PHP

---
+++ Curso_em_video/php_moderno/ex000/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando bla bla</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo  "la bla  \u{1F480} \u{2705} ";
+        ?>  
+    </h1>
+    <p>texto bla bla bla </p>
+    
+</body>
+</html>
